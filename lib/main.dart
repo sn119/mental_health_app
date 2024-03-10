60e090a9aac3db3a7154fa2f71cf9ae067f51e52
sri nori <snori4@ucmerced.edu>
2024-03-10 04:47:26 -0700

    making another applicatipon for breathing sessions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -137,7 +138,7 @@ class WelcomePage extends StatelessWidget{
             onPressed: (){
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => MyJournalScreen()),
+                MaterialPageRoute(builder: (context) => _FeelingPageState()),
               );
             },
 
@@ -147,8 +148,195 @@ class WelcomePage extends StatelessWidget{
                 borderRadius: BorderRadius.circular(10),
               )
             ),
-            child: Text("Start your Journal", style: TextStyle(fontSize: 48)),
+            child: Text("Start your Journey", style: TextStyle(fontSize: 48)),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+//How are u feeling page
+
+class _FeelingPageState extends StatefulWidget {
+  @override
+  __FeelingPageStateState createState() => __FeelingPageStateState();
+}
+
+class __FeelingPageStateState extends State <_FeelingPageState> {
+  int _counter = 0;
+  String _feeling = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 158, 201, 220),
+      appBar: AppBar(
+        title: Text('How are you feeling today?'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(right: 40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  print('button pressed!');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => JournalPage()),
+                  );
+                },
+
+                child: Text('Next'),
+
+              ),
+              RichText(
+                text: TextSpan(
+                  text: 'How are you feeling today? \n ', 
+                  //text: '      ',
+                  style: TextStyle(fontFamily: 'Roboto', fontSize: 50, color: Color.fromARGB(255, 58, 75, 86)),
+                  children: [
+                    WidgetSpan(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _feeling = '😁';
+                          });
+                        },
+                        child: Text('          😁', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 247, 0))),
+                      ),
+                    ),                  
+                    WidgetSpan(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _feeling = '😊';
+                          });
+                        },
+                        child: Text('😊', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 247, 0))),
+                      ),
+                    ),
+                    WidgetSpan(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _feeling = '😢';
+                          });
+                        },
+                        child: Text('😢', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 247, 0))),
+                      ),
+                    ),
+                    WidgetSpan(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _feeling = '😭';
+                          });
+                        },
+                        child: Text('😭', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 247, 0))),
+                      ),
+                    ),
+
+                    WidgetSpan(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _feeling = '😡';
+                          });
+                        },
+                        child: Text('😡', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 247, 0))),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'You are feeling: $_feeling',
+                style: TextStyle(fontSize: 50, color: Color.fromARGB(255, 127, 128, 128)),
+              ),
+            ],
+          ),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+}
+
+
+
+//breathing session page
+class _BreathingSession extends StatefulWidget {
+  @override
+  __BreathingSessionState createState() => __BreathingSessionState();
+}
+
+class __BreathingSessionState extends State <_BreathingSession>{
+  int counter  = 0;
+  int maxTime = 30;
+  String message = '';
+
+  @override
+  void inittState(){
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (timer)){
+      if (counter < maxTime){
+        if (counter % 10 == 0){
+          setMessage('Breath In');
+        }
+        else if (counter % 10 == 5){
+          setMessage('Breath Out');
+        }
+        counter += 1;
+      } 
+      else{
+        timer.cancel();
+        setMessage('Breathing Session Completed 🧘')
+      }
+    });
+  }
+  void setMessage(String newMessage){
+    setState(() {
+      message = newText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Breathing Session'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Breathing session in progress...',
+              style: TextStyle(fontsize:30),
+            ),
+            SizedBox(height: 20),
+            Text(
+              message,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
