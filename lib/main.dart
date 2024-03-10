b4ed1873f836d97bd27dc351801ff0d0155d51de
gowri <gsaravanan@ucmerced.edu>
2024-03-10 00:36:52 -0800

    coded final page, testing now

--- lib/main.dart
+++ lib/main.dart
@@ -1,301 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-<<<<<<< HEAD
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-=======
-  runApp(MyJournalApp());
-}
-
-class MyJournalApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: MyJournalScreen(),
-    );
-  }
-}
-
-class MyJournalScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Journal'),
-      ),
-      body: JournalPage(),
-    );
-  }
-}
-
-class JournalPage extends StatefulWidget{
-  @override
-  _JournalPageState createState() => _JournalPageState();
-}
-
-class _JournalPageState extends State<JournalPage>{
-  TextEditingController _answerController = TextEditingController();
-  TextEditingController _answerController2 = TextEditingController();
-  String _question = "What's on your mind today?";
-  String _question2 = "What's something new you learned/discovered today?";
-
-  @override 
-  Widget build(BuildContext context){
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/journal.png'),
-          fit: BoxFit.cover,
-
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: _buildJournalContent(),
-      ),
-    );
-  }
-
-  Widget _buildJournalContent(){
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Card(
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                _question,
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _answerController,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Type your answer here...',
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                _question2,
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _answerController,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Type your answer here...',
-                ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  print('User Answer 1: ${_answerController.text}');
-                  print('User Answer 1: ${_answerController2.text}');
-
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BreathingPage()),
-                  );
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose(){
-    _answerController.dispose();
-    _answerController2.dispose();
-    super.dispose();
-  }
-}
-
-class BreathingPage extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Breathing Page'),
-      ),
-      body: Center(
-        child: Text("This is the breathing page!"),
-      ),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-/*class MyApp extends StatelessWidget {
->>>>>>> origin/main
-  const MyApp({super.key});
+  //const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-<<<<<<< HEAD
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-=======
->>>>>>> origin/main
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Text Page Example',
+      home: FinalPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-<<<<<<< HEAD
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-=======
->>>>>>> origin/main
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-<<<<<<< HEAD
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-=======
->>>>>>> origin/main
-      _counter++;
-    });
-  }
-
+class FinalPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Final Page'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-=======
-    
-    return Scaffold(
-      appBar: AppBar(
-      
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
->>>>>>> origin/main
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Text(
+          'Are you feeling better?',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-<<<<<<< HEAD
 }
-=======
-}*/
-
->>>>>>> origin/main
